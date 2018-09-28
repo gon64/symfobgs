19bb9d777ec84e681f6aab0e528ba8382242e749
gon <gonzaloventura@outlook.com>
2018-09-28 15:15:25 +0000

    start security settings

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,22 +4,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\Usuario;
 
 class SecurityController extends AbstractController
 {
 	/**
 	 * @Route("/login", name="login")
 	 */
-	public function login(AuthenticationUtils $authenticationUtils)	{
-		// get the login error if there is one
+	public function loginAction(AuthenticationUtils $authenticationUtils)	{
+//		$authenticationUtils = $this->get('security.authentication_utils');
 		$error = $authenticationUtils->getLastAuthenticationError();
-		
-		// last username entered by the user
 		$lastUsername = $authenticationUtils->getLastUsername();
-
 		return $this->render('security/login.html.twig', array(
 			'last_username' => $lastUsername,
 			'error'         => $error,
 		));
+	}
+
+	/**
+	 * @Route("/registro")
+	 */
+	public function registerAction() {
+		$user = new Usuario();
+		return $this->render(
+			'security/register.html.twig'
+		);
 	}
 }
--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -9,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsuarioRepository")
  */
-class Usuario
+class Usuario implements UserInterface 
 {
     /**
      * @ORM\Id()
@@ -148,4 +149,35 @@ class Usuario
 
         return $this;
     }
+
+    /*
+     * Metodos de la interfaz UserInterface
+     */
+    public function getPassword()
+    {
+        return $this->clave;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+ 
+    public function getSalt()
+    {
+        return null;
+    }
+ 
+    public function getUsername()
+    {
+        return $this->usuario;
+    }
+ 
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+
+
 }
