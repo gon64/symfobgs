10d95721e07deb525cf9b331f625f6582a34f3cb
gon <gonzaloventura@outlook.com>
2018-10-31 16:05:08 +0000

    add email/username login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,7 +19,6 @@ class SecurityController extends Controller
 	 * @Route("/login", name="login")
 	 */
 	public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)	{
-//		$authenticationUtils = $this->get('security.authentication_utils');
 		$error = $authenticationUtils->getLastAuthenticationError();
 		$lastUsername = $authenticationUtils->getLastUsername();
 		return $this->render('security/login.html.twig', [
--- src/Repository/UsuarioRepository.php
+++ src/Repository/UsuarioRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Usuario;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
 /**
  * @method Usuario|null find($id, $lockMode = null, $lockVersion = null)
@@ -12,12 +13,24 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method Usuario[]    findAll()
  * @method Usuario[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UsuarioRepository extends ServiceEntityRepository
+class UsuarioRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Usuario::class);
     }
+
+public function loadUserByUsername($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.usuario = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+
 
 //    /**
 //     * @return Usuario[] Returns an array of Usuario objects
